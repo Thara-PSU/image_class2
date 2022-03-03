7c14b95d89b5cbe9847cebe8a1c5a008bda7bea0
Thara-PSU <73512825+Thara-PSU@users.noreply.github.com>
2022-03-04 06:35:10 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,95 @@
+# importing the libraries
+import streamlit as st
+import joblib
+from PIL import Image
+from skimage.transform import resize
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import time
+
+
+# loading the cat classifier model
+HOG_model = joblib.load("HOG_model.pkl")
+
+
+# functions to predict image
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+
+    return s
+
+
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+
+    # Compute the probability of a cat being present in the picture
+
+    Y_prediction = sigmoid((np.dot(w.T, X) + b))
+
+    return Y_prediction
+
+
+# Designing the interface
+st.title("Butterfly Tumor Image Classification App")
+# For newline
+st.write('\n')
+
+image = Image.open('images/image.png')
+show = st.image(image, use_column_width=True)
+
+st.sidebar.title("Upload Image")
+
+# Disabling warning
+st.set_option('deprecation.showfileUploaderEncoding', False)
+# Choose your own image
+uploaded_file = st.sidebar.file_uploader(" ", type=['png', 'jpg', 'jpeg'])
+
+if uploaded_file is not None:
+    u_img = Image.open(uploaded_file)
+    show.image(u_img, 'Uploaded Image', use_column_width=True)
+    # We preprocess the image to fit in algorithm.
+    image = np.asarray(u_img) / 255
+
+    my_image = resize(image, (64, 64)).reshape((1, 64 * 64 * 3)).T
+
+
+    
+# For newline
+st.sidebar.write('\n')
+
+if st.sidebar.button("Click Here to Classify"):
+
+    if uploaded_file is None:
+
+        st.sidebar.write("Please upload an Image to Classify")
+
+    else:
+
+        with st.spinner('Classifying ...'):
+
+            prediction = predict(HOG_model["w"], GBM_knn_clf_model["b"], my_image)
+            #prediction = GBM_knn_clf_model.predict(my_image)
+            time.sleep(2)
+            st.success('Done!')
+
+        st.sidebar.header("Algorithm Predicts: ")
+
+        # Formatted probability value to 3 decimal places
+        probability = "{:.3f}".format(float(prediction * 100))
+
+        # Classify PCNSL being present in the picture if prediction > 0.5
+
+        if prediction > 0.5:
+
+            st.sidebar.write("It's a 'PCNSL' picture.", '\n')
+
+            st.sidebar.write('**Probability: **', probability, '%')
+
+
+        else:
+            st.sidebar.write(" It's a 'GBM' picture ", '\n')
+
+            st.sidebar.write('**Probability: **', probability, '%')
